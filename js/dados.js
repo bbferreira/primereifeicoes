d07b0f619edff37fccd4e2c98b9287614221d432
bbferreirar <brunobocardiferreiraite@gmail.com>
2024-07-12 14:03:56 -0300

    Commited

--- js/dados.js
+++ js/dados.js
@@ -1,250 +1,246 @@
 var MENU = {
-    "churrasco": [
-        {
-            "id": "ribs-brisket-and-burnt-ends",
-            "img": "./img/cardapio/churrasco/FILÉ DE FRANGO A PARMEGIANA.jpeg",
-            "name": "FILÉ DE FRANGO A PARMEGIANA",
-            "dsc": " Parmegiana de frango, arroz, fritas e salada",
-            "price": 27.90
-        },
-        {
-            "id": "005-kings-carolina-oink-sampler",
-            "img": "./img/cardapio/churrasco/FILÉ DE FRANGO EMPANADO.jpeg",
-            "name": "FILÉ DE FRANGO EMPANADO",
-            "dsc": "Frango empanado, arroz, feijão, fritas e salada",
-            "price": 22.90
-        },
-        {
-            "id": "texas-monthlys-1-bbq-brisket",
-            "img": "./img/cardapio/churrasco/FILÉ DE FRANGO GRELHADO.jpeg",
-            "name": "FILÉ DE FRANGO GRELHADO",
-            "dsc": "Frango grelhado, arroz, feijão, fritas e salada",
-            "price": 22.90
-        },
-        {
-            "id": "whole-brisket-texas-bbq-sauce",
-            "img": "./img/cardapio/churrasco/SALADA TROPICAL COM FILÉ DE FRANGO.jpg",
-            "name": "SALADA TROPICAL COM FILÉ DE FRANGO",
-            "dsc": "Salada mista com frango grelhado",
-            "price": 22.90
-        },
-        {
-            "id": "whole-texas-smoked-brisket",
-            "img": "./img/cardapio/churrasco/STROGONOFF DE FRANGO.jpeg",
-            "name": "STROGONOFF DE FRANGO",
-            "dsc": "Strogonoff de frango, arroz, fritas, batata palha e salada",
-            "price": 25.90
-        },
-    ],
+  churrasco: [
+    {
+      id: "ribs-brisket-and-burnt-ends",
+      img: "./img/cardapio/churrasco/FILÉ DE FRANGO A PARMEGIANA.jpeg",
+      name: "FILÉ DE FRANGO A PARMEGIANA",
+      dsc: " Parmegiana de frango, arroz, fritas e salada",
+      price: 27.9,
+    },
+    {
+      id: "005-kings-carolina-oink-sampler",
+      img: "./img/cardapio/churrasco/FILÉ DE FRANGO EMPANADO.jpeg",
+      name: "FILÉ DE FRANGO EMPANADO",
+      dsc: "Frango empanado, arroz, feijão, fritas e salada",
+      price: 22.9,
+    },
+    {
+      id: "texas-monthlys-1-bbq-brisket",
+      img: "./img/cardapio/churrasco/FILÉ DE FRANGO GRELHADO.jpeg",
+      name: "FILÉ DE FRANGO GRELHADO",
+      dsc: "Frango grelhado, arroz, feijão, fritas e salada",
+      price: 22.9,
+    },
+    {
+      id: "whole-brisket-texas-bbq-sauce",
+      img: "./img/cardapio/churrasco/SALADA TROPICAL COM FILÉ DE FRANGO.jpg",
+      name: "SALADA TROPICAL COM FILÉ DE FRANGO",
+      dsc: "Salada mista com frango grelhado",
+      price: 22.9,
+    },
+    {
+      id: "whole-texas-smoked-brisket",
+      img: "./img/cardapio/churrasco/STROGONOFF DE FRANGO.jpeg",
+      name: "STROGONOFF DE FRANGO",
+      dsc: "Strogonoff de frango, arroz, fritas, batata palha e salada",
+      price: 25.9,
+    },
+  ],
 
-    "burgers": [
-        {
-            "id": "Omelete",
-            "img": "./img/cardapio/burguers/2 OVOS FRITOS.jpeg",
-            "name": "2 OVOS FRITOS",
-            "dsc": "2 Ovos Fritos, arroz, feijão, fritas e salada",
-            "price": 16.90
-        },
-        {
-            "id": "Omeleete",
-            "img": "img/cardapio/burguers/OMELETE COM SALADA.jpg",
-            "name": "OMELETE COM SALADA",
-            "dsc": "Omelete de quatro ovos com dois recheios da sua escolha e salada",
-            "price": 22.90
-        },
-        {
-            "id": "OMELETE",
-            "img": "./img/cardapio/burguers/OMELETE.jpeg",
-            "name": "OMELETE",
-            "dsc": "Omelete de dois ovos, arroz, feijão, fritas e salada",
-            "price": 16.90
-        },
-        {
-            "id": "shake-shack-shackburger-16-pack",
-            "img": "./img/cardapio/burguers/OMELETE COM MUSSARELA.jpeg",
-            "name": "OMELETE COM MUSSARELA",
-            "dsc": "Omelete com dois ovos e mussarela, arroz, feijão, fritas e salada",
-            "price": 19.90
-        },
-    ],
-    "sobremesas": [
-        {
-            "id": "15259-german-chocolate-killer-brownie-tin-pack",
-            "img": "./img/cardapio/sobremesas/MACARRÃO ALHO E OLEO COM FILÉ DE FRANGO.jpg",
-            "name": "MACARRÃO ALHO E OLEO COM FILÉ DE FRANGO",
-            "dsc": " Macarrão alho e óleo com frango grelhado ",
-            "price": 22.90
-        },
-        {
-            "id": "jacques-world-famous-chocolate-chip-cookies",
-            "img": "./img/cardapio/sobremesas/MASSA ITALIANA AL SUGO.jpg",
-            "name": "MASSA ITALIANA AL SUGO",
-            "dsc": " Macarrão tipo grano duro com molho de tomates",
-            "price": 19.90
-        },
-        {
-            "id": "luigis-original-cannoli-pie",
-            "img": "./img/cardapio/sobremesas/MASSA ITALIANA COM ALMONDEGAS.jpeg",
-            "name": "MASSA ITALIANA COM ALMONDEGAS",
-            "dsc": "Macarrão grano duro com molho de tomate e almondegas",
-            "price": 22.90
-        },
-        
-    ],
-    "bebidas": [
-        {
-            "id": "hong-kong-boba-tea-kit-for-6",
-            "img": "./img/cardapio/bebidas/BISTECA SUINA COM OVO.jpeg",
-            "name": "BISTECA SUINA COM OVO",
-            "dsc": " Bisteca de porco, ovo frito, arroz, feijão, fritas e salada",
-            "price": 25.90
-        },
-        {
-            "id": "guys-caliente-margaritas-for-12",
-            "img": "./img/cardapio/bebidas/Bisteca Suína.jpeg",
-            "name": "Bisteca Suína",
-            "dsc": "Bisteca de porco, arroz, feijão, fritas e salada",
-            "price": 22.90
-        },
-        {
-            "id": "woodford-reserve-mint-julep-syrup",
-            "img": "./img/cardapio/bebidas/CALABRESA.jpeg",
-            "name": "CALABRESA",
-            "dsc": " Calabresa em rodelas, arroz, feijão, fritas e salada",
-            "price": 19.90
-        },
-        {
-            "id": "new-orleans-hurricane-mix",
-            "img": "./img/cardapio/bebidas/FILÉ DE PEIXE EMPANADO.jpg",
-            "name": "FILÉ DE PEIXE EMPANADO",
-            "dsc": "Filé de peixe empanado, arroz, feijão, fritas e salada",
-            "price": 29.90
-        },
-        {
-            "id": "margarita-mix",
-            "img": "./img/cardapio/bebidas/HAMBURGER VEGANO.jpg",
-            "name": "HAMBURGER VEGANO",
-            "dsc": " Hamburger vegano grelhado, arroz, feijão, fritas e salada",
-            "price": 26.90
-        },
-        {
-            "id": "woodford-reserve-mint-julep-syrup-2-pack",
-            "img": "./img/cardapio/bebidas/HAMBURGER VEGETARARIANO A PARMEGIANA.jpg",
-            "name": "HAMBURGER VEGETARARIANO A PARMEGIANA",
-            "dsc": " Parmegiana, arroz, fritas e salada",
-            "price": 29.90
-        },
-        {
-            "id": "unicorn-parade-milkshake-kit-for-8",
-            "img": "./img/cardapio/bebidas/LINGUIÇA DE FRANGO COM BATATA.jpg",
-            "name": "LINGUIÇA DE FRANGO COM BATATA",
-            "dsc": " Linguiça de frango frita, arroz, feijão, fritas e salada",
-            "price": 18.90
-        },
-        {
-            "id": "chickpea-chiller-kit-for-6",
-            "img": "./img/cardapio/bebidas/LINGUIÇA DE FRANGO COM FAROFA.jpg",
-            "name": "LINGUIÇA DE FRANGO COM FAROFA",
-            "dsc": " Linguiça de frango frita, arroz, feijão, farofa e salada",
-            "price": 16.90
-        },
-        {
-            "id": "15194-old-honey-barn-mint-julep-mixer-200ml",
-            "img": "./img/cardapio/bebidas/LINGUIÇA TOSCANA ESPECIAL.jpeg",
-            "name": "LINGUIÇA TOSCANA ESPECIAL",
-            "dsc": "Linguiça toscana frita, arroz, feijão, fritas e salada",
-            "price": 19.90
-        },
-        {
-            "id": "kentucky-derby-mint-julep-gift-set",
-            "img": "./img/cardapio/bebidas/PÃO DE QUEIJO NO PRATO.jpg",
-            "name": "PÃO DE QUEIJO NO PRATO",
-            "dsc": " Pão de queijo no prato com dois recheios a sua escolha",
-            "price": 22.90
-        },
-        {
-            "id": "002-charleston-bloody-mary-mix-weekender-bold-and-spicy",
-            "img": "./img/cardapio/bebidas/SALSICHA.jpeg",
-            "name": "SALSICHA",
-            "dsc": "Salsicha com molho de tomate, arroz, feijão, fritas e salada ",
-            "price": 16.90
-        },
-
-    ],
-    "pizzas": [
-        {
-            "id": "2-lou-malnatis-deep-dish-pizzas",
-            "img": "./img/cardapio/pizzas/ALMONDEGA.jpeg",
-            "name": "ALMONDEGA",
-            "dsc": "Almondega com molho de tomate, arroz, feijão, fritas e salada",
-            "price": 22.90
-        },
-        {
-            "id": "23699-choose-your-own-thin-crust-pizza-4-pack",
-            "img": "./img/cardapio/pizzas/HAMBURGER COM OVO.jpeg",
-            "name": "HAMBURGER COM OVO",
-            "dsc": "Hamburger, ovo frito, arroz, feijão, fritas e salada",
-            "price": 22.90
-        },
-        {
-            "id": "choose-your-own-new-haven-style-pizza-6-pack",
-            "img": "./img/cardapio/pizzas/HAMBURGER.jpeg",
-            "name": "HAMBURGER",
-            "dsc": "Hamburger grelhado, arroz, feijão, fritas e salada",
-            "price": 18.90
-        },
-        {
-            "id": "6-lou-malnatis-deep-dish-pizzas",
-            "img": "./img/cardapio/pizzas/PICADINHO NA MANTEIGA.jpeg",
-            "name": "PICADINHO NA MANTEIGA",
-            "dsc": "Carne picadinha na manteiga, arroz, feijão, fritas e salada",
-            "price": 22.90
-        },
-        
-    ],
-    "steaks": [
-        {
-            "id": "california-reserve-filet-mignon-steaks-gift-box",
-            "img": "./img/cardapio/steaks/BIFE NA CHAPA COM OVO.jpeg",
-            "name": "BIFE NA CHAPA COM OVO",
-            "dsc": " Bife, ovo frito, arroz, feijão, fritas e salada ",
-            "price": 32.90
-        },
-        {
-            "id": "steaks-and-cakes-date-night-dinner-for-2",
-            "img": "./img/cardapio/steaks/BIFE NA CHAPA.jpeg",
-            "name": "BIFE NA CHAPA",
-            "dsc": "Bife, arroz, feijão, fritas e salada",
-            "price": 29.90
-        },
-        {
-            "id": "Prime-holiday-steak-sampler-for-10-12",
-            "img": "./img/cardapio/steaks/FILÉ DE CARNE A PARMEGIANA.jpeg",
-            "name": "FILÉ DE CARNE A PARMEGIANA",
-            "dsc": " Parmegiana de carne, arroz, fritas e salada",
-            "price": 32.90
-        },
-        {
-            "id": "bone-in-rib-steak",
-            "img": "./img/cardapio/steaks/FILÉ DE CARNE EMPANADO.jpg",
-            "name": "FILÉ DE CARNE EMPANADO",
-            "dsc": "Filé empanado, arroz, feijão, fritas e salada",
-            "price": 28.90
-        },
-        {
-            "id": "american-wagyu-gold-grade-top-sirloins",
-            "img": "./img/cardapio/steaks/PICANHA NA CHAPA.jpg",
-            "name": "PICANHA NA CHAPA",
-            "dsc": " Picanha grelhada, arroz, feijão, fritas e salada",
-            "price": 32.90
-        },
-        {
-            "id": "2-peter-luger-steak-pack-b",
-            "img": "./img/cardapio/steaks/STROGONOFF DE CARNE.jpeg",
-            "name": "STROGONOFF DE CARNE",
-            "dsc": "Strogonoff de carne, arroz, fritas, batata palha e salada",
-            "price": 29.90
-        },
-
-    ]
-}
+  burgers: [
+    {
+      id: "Omelete",
+      img: "./img/cardapio/burguers/2 OVOS FRITOS.jpeg",
+      name: "2 OVOS FRITOS",
+      dsc: "2 Ovos Fritos, arroz, feijão, fritas e salada",
+      price: 16.9,
+    },
+    {
+      id: "Omeleete",
+      img: "img/cardapio/burguers/OMELETE COM SALADA.jpg",
+      name: "OMELETE COM SALADA",
+      dsc: "Omelete de quatro ovos com dois recheios da sua escolha e salada",
+      price: 22.9,
+    },
+    {
+      id: "OMELETE",
+      img: "./img/cardapio/burguers/OMELETE.jpeg",
+      name: "OMELETE",
+      dsc: "Omelete de dois ovos, arroz, feijão, fritas e salada",
+      price: 16.9,
+    },
+    {
+      id: "shake-shack-shackburger-16-pack",
+      img: "./img/cardapio/burguers/OMELETE COM MUSSARELA.jpeg",
+      name: "OMELETE COM MUSSARELA",
+      dsc: "Omelete com dois ovos e mussarela, arroz, feijão, fritas e salada",
+      price: 19.9,
+    },
+  ],
+  sobremesas: [
+    {
+      id: "15259-german-chocolate-killer-brownie-tin-pack",
+      img: "./img/cardapio/sobremesas/MACARRÃO ALHO E OLEO COM FILÉ DE FRANGO.jpg",
+      name: "MACARRÃO ALHO E OLEO COM FILÉ DE FRANGO",
+      dsc: " Macarrão alho e óleo com frango grelhado ",
+      price: 22.9,
+    },
+    {
+      id: "jacques-world-famous-chocolate-chip-cookies",
+      img: "./img/cardapio/sobremesas/MASSA ITALIANA AL SUGO.jpg",
+      name: "MASSA ITALIANA AL SUGO",
+      dsc: " Macarrão tipo grano duro com molho de tomates",
+      price: 19.9,
+    },
+    {
+      id: "luigis-original-cannoli-pie",
+      img: "./img/cardapio/sobremesas/MASSA ITALIANA COM ALMONDEGAS.jpeg",
+      name: "MASSA ITALIANA COM ALMONDEGAS",
+      dsc: "Macarrão grano duro com molho de tomate e almondegas",
+      price: 22.9,
+    },
+  ],
+  bebidas: [
+    {
+      id: "hong-kong-boba-tea-kit-for-6",
+      img: "./img/cardapio/bebidas/BISTECA SUINA COM OVO.jpeg",
+      name: "BISTECA SUINA COM OVO",
+      dsc: " Bisteca de porco, ovo frito, arroz, feijão, fritas e salada",
+      price: 25.9,
+    },
+    {
+      id: "guys-caliente-margaritas-for-12",
+      img: "./img/cardapio/bebidas/BISTECA SUÍNA.jpeg",
+      name: "Bisteca Suína",
+      dsc: "Bisteca de porco, arroz, feijão, fritas e salada",
+      price: 22.9,
+    },
+    {
+      id: "woodford-reserve-mint-julep-syrup",
+      img: "./img/cardapio/bebidas/CALABRESA.jpeg",
+      name: "CALABRESA",
+      dsc: " Calabresa em rodelas, arroz, feijão, fritas e salada",
+      price: 19.9,
+    },
+    {
+      id: "new-orleans-hurricane-mix",
+      img: "./img/cardapio/bebidas/FILÉ DE PEIXE EMPANADO.jpg",
+      name: "FILÉ DE PEIXE EMPANADO",
+      dsc: "Filé de peixe empanado, arroz, feijão, fritas e salada",
+      price: 29.9,
+    },
+    {
+      id: "margarita-mix",
+      img: "./img/cardapio/bebidas/HAMBURGER VEGANO.jpg",
+      name: "HAMBURGER VEGANO",
+      dsc: " Hamburger vegano grelhado, arroz, feijão, fritas e salada",
+      price: 26.9,
+    },
+    {
+      id: "woodford-reserve-mint-julep-syrup-2-pack",
+      img: "./img/cardapio/bebidas/HAMBURGER VEGETARARIANO A PARMEGIANA.jpg",
+      name: "HAMBURGER VEGETARARIANO A PARMEGIANA",
+      dsc: " Parmegiana, arroz, fritas e salada",
+      price: 29.9,
+    },
+    {
+      id: "unicorn-parade-milkshake-kit-for-8",
+      img: "./img/cardapio/bebidas/LINGUIÇA DE FRANGO COM BATATA.jpg",
+      name: "LINGUIÇA DE FRANGO COM BATATA",
+      dsc: " Linguiça de frango frita, arroz, feijão, fritas e salada",
+      price: 18.9,
+    },
+    {
+      id: "chickpea-chiller-kit-for-6",
+      img: "./img/cardapio/bebidas/LINGUIÇA DE FRANGO COM FAROFA.jpg",
+      name: "LINGUIÇA DE FRANGO COM FAROFA",
+      dsc: " Linguiça de frango frita, arroz, feijão, farofa e salada",
+      price: 16.9,
+    },
+    {
+      id: "15194-old-honey-barn-mint-julep-mixer-200ml",
+      img: "./img/cardapio/bebidas/LINGUIÇA TOSCANA ESPECIAL.jpeg",
+      name: "LINGUIÇA TOSCANA ESPECIAL",
+      dsc: "Linguiça toscana frita, arroz, feijão, fritas e salada",
+      price: 19.9,
+    },
+    {
+      id: "kentucky-derby-mint-julep-gift-set",
+      img: "./img/cardapio/bebidas/PÃO DE QUEIJO NO PRATO.jpg",
+      name: "PÃO DE QUEIJO NO PRATO",
+      dsc: " Pão de queijo no prato com dois recheios a sua escolha",
+      price: 22.9,
+    },
+    {
+      id: "002-charleston-bloody-mary-mix-weekender-bold-and-spicy",
+      img: "./img/cardapio/bebidas/SALSICHA.jpeg",
+      name: "SALSICHA",
+      dsc: "Salsicha com molho de tomate, arroz, feijão, fritas e salada ",
+      price: 16.9,
+    },
+  ],
+  pizzas: [
+    {
+      id: "2-lou-malnatis-deep-dish-pizzas",
+      img: "./img/cardapio/pizzas/ALMONDEGA.jpeg",
+      name: "ALMONDEGA",
+      dsc: "Almondega com molho de tomate, arroz, feijão, fritas e salada",
+      price: 22.9,
+    },
+    {
+      id: "23699-choose-your-own-thin-crust-pizza-4-pack",
+      img: "./img/cardapio/pizzas/HAMBURGER COM OVO.jpeg",
+      name: "HAMBURGER COM OVO",
+      dsc: "Hamburger, ovo frito, arroz, feijão, fritas e salada",
+      price: 22.9,
+    },
+    {
+      id: "choose-your-own-new-haven-style-pizza-6-pack",
+      img: "./img/cardapio/pizzas/HAMBURGER.jpeg",
+      name: "HAMBURGER",
+      dsc: "Hamburger grelhado, arroz, feijão, fritas e salada",
+      price: 18.9,
+    },
+    {
+      id: "6-lou-malnatis-deep-dish-pizzas",
+      img: "./img/cardapio/pizzas/PICADINHO NA MANTEIGA.jpeg",
+      name: "PICADINHO NA MANTEIGA",
+      dsc: "Carne picadinha na manteiga, arroz, feijão, fritas e salada",
+      price: 22.9,
+    },
+  ],
+  steaks: [
+    {
+      id: "california-reserve-filet-mignon-steaks-gift-box",
+      img: "./img/cardapio/steaks/BIFE NA CHAPA COM OVO.jpeg",
+      name: "BIFE NA CHAPA COM OVO",
+      dsc: " Bife, ovo frito, arroz, feijão, fritas e salada ",
+      price: 32.9,
+    },
+    {
+      id: "steaks-and-cakes-date-night-dinner-for-2",
+      img: "./img/cardapio/steaks/BIFE NA CHAPA.jpeg",
+      name: "BIFE NA CHAPA",
+      dsc: "Bife, arroz, feijão, fritas e salada",
+      price: 29.9,
+    },
+    {
+      id: "Prime-holiday-steak-sampler-for-10-12",
+      img: "./img/cardapio/steaks/FILÉ DE CARNE A PARMEGIANA.jpeg",
+      name: "FILÉ DE CARNE A PARMEGIANA",
+      dsc: " Parmegiana de carne, arroz, fritas e salada",
+      price: 32.9,
+    },
+    {
+      id: "bone-in-rib-steak",
+      img: "./img/cardapio/steaks/FILÉ DE CARNE EMPANADO.jpg",
+      name: "FILÉ DE CARNE EMPANADO",
+      dsc: "Filé empanado, arroz, feijão, fritas e salada",
+      price: 28.9,
+    },
+    {
+      id: "american-wagyu-gold-grade-top-sirloins",
+      img: "./img/cardapio/steaks/PICANHA NA CHAPA.jpg",
+      name: "PICANHA NA CHAPA",
+      dsc: " Picanha grelhada, arroz, feijão, fritas e salada",
+      price: 32.9,
+    },
+    {
+      id: "2-peter-luger-steak-pack-b",
+      img: "./img/cardapio/steaks/STROGONOFF DE CARNE.jpeg",
+      name: "STROGONOFF DE CARNE",
+      dsc: "Strogonoff de carne, arroz, fritas, batata palha e salada",
+      price: 29.9,
+    },
+  ],
+};
